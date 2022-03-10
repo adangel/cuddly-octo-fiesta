d2948fc6cd1f26fd221cac4eb63c19f271848c08
GitHub Action <action@github.com>
2022-03-10 18:58:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/adangel/cuddlyoctofiesta/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/adangel/cuddlyoctofiesta/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.adangel.cuddlyoctofiesta.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adangel.cuddlyoctofiesta.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/adangel/cuddlyoctofiesta/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adangel/cuddlyoctofiesta/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.adangel.cuddlyoctofiesta.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adangel.cuddlyoctofiesta.MyBundle
 
 class MyProjectService(project: Project) {
 
